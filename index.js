d482c7f9ba597be14ccc1946d6a49e304f07f21e
unknown <gsabolova5@gmail.com>
2024-04-02 16:02:25 +0200

    Modified variables to fit instruction

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const round = (number) => {
-  let decimalPoint = number.indexOf(".");
-  let numberDecimals = number.slice(decimalPoint, -1);
   let numberInt = Math.trunc(number);
+  let numberDecimal = number - numberInt;
+  let result;
 
-  if (numberDecimals < 0.5) {
+  if (numberDecimal < 0.5) {
     result = numberInt;
-  } else if (numberDecimals > 0.5) {
-    result = numberInt + 1;
-  } else if (numberDecimals === 0.5) {
-      if (numberInt % 2 === 0) {
-      result = numberInt - 1;
-      } else {
+  } else if (numberDecimal > 0.5) {
+    result = Math.ceil(number);
+  } else if (numberDecimal === 0.5) {
+    if (numberInt % 2 === 0) {
+      result = numberInt;
+    } else {
       result = numberInt + 1;
-      }
+    }
   } else {
-    result = "Toto není číslo."
+    result = "Toto není číslo.";
   }
   return result;
 };
 
-const userInput = prompt(
-  "Zadejte číslo, které chcete zaokrouhlit. Použijde desetinnou tečku."
+const userInput = Number(
+  prompt("Zadejte číslo, které chcete zaokrouhlit. Použijde desetinnou tečku.")
 );
 const roundedNumber = round(userInput);
 document.body.innerHTML = userInput + " je zaokrouhleno na " + roundedNumber;
-
-
